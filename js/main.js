0fcda4c76a9b7eef4534642fca4b8fca73adc1ae
Ahmed Mohamed Gaber <ahmed.mohamed.gaber@gmail.com>
2018-07-11 21:10:41 +0200

    responsive CSS

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYWhtZWRtb2hhbWVkZ2FiZXIiLCJhIjoiY2pqZ2FoeGxrMXYxZDN3b2xrNmN4d2hrciJ9.l_UrBsnlqr5qhoMkWSdEKw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
